edf4e70a8d0140f1b1a7494be61c94b80c938546
Joshua <email@gmail.com>
2024-05-21 13:14:31 +0200

    Validacion inicial

--- proyecto_git_JOSHUA_RAMIREZ/src/git/ClaseRamaMaster.java
+++ proyecto_git_JOSHUA_RAMIREZ/src/git/ClaseRamaMaster.java
@@ -3,6 +3,6 @@ package git;
 public class ClaseRamaMaster {
 
 	public static void main(String[] args) {
-		System.out.println("Primer fichero del paquete git en la rama Master");
+		System.out.println("Prime fichero del paquete git en la rama Master");
 	}
 }
